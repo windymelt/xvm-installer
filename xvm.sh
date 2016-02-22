605fa6914fa28fe8d1f3492852a47a01206c836d
Keisuke Noguchi <windy.imlovinit@gmail.com>
2016-02-23 02:19:42 +0900

    fixed bug

--- xvm.sh
+++ xvm.sh
@@ -13,14 +13,14 @@ if [ ! -d $WOT_ROOT ]; then
 fi
 
 echo "Downloading XVM..."
-curl http://dl1.modxvm.com/get/bin/xvm-6.2.0.1.zip -e http://dl1.modxvm.com/bin/xvm-6.2.0.1.zip -o xvm.zip || { echo "Download error. Exiting." ; exit 1 }
+curl http://dl1.modxvm.com/get/bin/xvm-6.2.0.1.zip -e http://dl1.modxvm.com/bin/xvm-6.2.0.1.zip -o xvm.zip || ( echo "Download error. Exiting." ; exit 1 )
 
 echo "Extracting compresed file..."
-unzip xvm.zip -d xvm || { echo "Unzip error. Exiting." ; rm xvm.zip ; exit 1 }
+unzip xvm.zip -d xvm || ( echo "Unzip error. Exiting." ; rm xvm.zip ; exit 1 )
 echo "Merging MOD..."
-rsync -abvuP ./xvm/res_mods/ $WOT_ROOT/res_mods/ || { echo "Rsync error. Exiting." ; exit 1 }
+rsync -abvuP ./xvm/res_mods/ $WOT_ROOT/res_mods/ || ( echo "Rsync error. Exiting." ; exit 1 )
 echo "Renaming: $WOT_ROOT/res_mods/configs/xvm/xvm.xc.sample"
 echo "to -> $WOT_ROOT/res_mods/configs/xvm/xvm.xc ..."
-mv $WOT_ROOT/res_mods/configs/xvm/xvm.xc.sample $WOT_ROOT/res_mods/configs/xvm/xvm.xc || { echo "Renaming error. Exiting." ; exit 1 }
+mv $WOT_ROOT/res_mods/configs/xvm/xvm.xc.sample $WOT_ROOT/res_mods/configs/xvm/xvm.xc || ( echo "Renaming error. Exiting." ; exit 1 )
 rm -rf xvm.zip xvm
 echo "Done! Panzer Vor!"
